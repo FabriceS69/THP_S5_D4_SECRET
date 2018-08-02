e1195d850456143c5027ed0da603dc32afc90276
FabriceS69 <fabrice.fullstack@gmail.com>
2018-08-02 19:56:31 +0200

    update static_pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  get 'static_pages/secret_page'
+  get '/', to 'static_pages/index'
+  get '/secret', to 'static_pages/secret_page'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
